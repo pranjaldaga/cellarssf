08e889d98327053748d59c9748fe502de5b65e1f
Philip I. Thomas <philip@moonlightwork.com>
2020-03-18 21:36:08 -0400

    add basic home screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,217 @@
 <template>
   <div>
-    <nuxt />
+    <div class="relative h-screen overflow-hidden bg-white">
+      <div class="max-w-screen-xl mx-auto ">
+        <div
+          class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+        >
+          <div class="px-4 pt-6 sm:px-6 lg:px-8">
+            <nav
+              class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+            >
+              <div
+                class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0"
+              >
+                <div class="flex items-center justify-between w-full md:w-auto">
+                  <nuxt-link to="/">
+                    <img
+                      class="w-auto h-8 sm:h-10"
+                      src="~/assets/logo.svg"
+                      alt="Cellar.NYC logo"
+                    />
+                  </nuxt-link>
+                  <div class="flex items-center -mr-2 md:hidden">
+                    <button
+                      type="button"
+                      class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+                      @click="open = true"
+                    >
+                      <svg
+                        class="w-6 h-6"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div class="hidden md:block md:ml-10 md:pr-4">
+                <a
+                  href="#"
+                  class="font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+                  >Product</a
+                >
+                <a
+                  href="#"
+                  class="ml-8 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+                  >Features</a
+                >
+                <a
+                  href="#"
+                  class="ml-8 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+                  >Marketplace</a
+                >
+              </div>
+            </nav>
+          </div>
+
+          <div
+            v-show="open"
+            x-transition:enter="duration-150 ease-out"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="duration-100 ease-in"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden"
+          >
+            <div class="rounded-lg shadow-md">
+              <div class="overflow-hidden bg-white rounded-lg shadow-xs">
+                <div class="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      class="w-auto h-8"
+                      src="/img/logos/workflow-mark-on-white.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div class="-mr-2">
+                    <button
+                      type="button"
+                      class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+                      @click="open = false"
+                    >
+                      <svg
+                        class="w-6 h-6"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div class="px-2 pt-2 pb-3">
+                  <a
+                    href="#"
+                    class="block px-3 py-2 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+                    >Product</a
+                  >
+                  <a
+                    href="#"
+                    class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+                    >Features</a
+                  >
+                  <a
+                    href="#"
+                    class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+                    >Marketplace</a
+                  >
+                  <a
+                    href="#"
+                    class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+                    >Company</a
+                  >
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    class="block w-full px-5 py-3 font-medium text-center transition duration-150 ease-in-out text-contraption-primary-600 bg-gray-50 hover:bg-gray-100 hover:text-contraption-primary-700 focus:outline-none focus:bg-gray-100 focus:text-contraption-primary-700"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="max-w-screen-xl px-4 mx-auto mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+          >
+            <div class="sm:text-center lg:text-left">
+              <h2
+                class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+              >
+                Save local restaurants by
+                <br class="xl:hidden" />
+                <span class="text-contraption-primary-600"
+                  >buying their wine</span
+                >
+              </h2>
+              <p
+                class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+              >
+                Your local restaurant has thousands of dollars of food and
+                beverage inventory sitting in their cellars. Help them stay
+                alive by buying it.
+              </p>
+              <div
+                class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
+              >
+                <div class="rounded-md shadow">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-contraption-primary-600 hover:bg-contraption-primary-500 focus:outline-none focus:shadow-outline md:py-4 md:text-lg md:px-10"
+                  >
+                    Find local restaurants
+                  </a>
+                </div>
+                <div class="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 transition duration-150 ease-in-out border border-transparent rounded-md text-contraption-primary-700 bg-contraption-primary-100 hover:text-contraption-primary-600 hover:bg-contraption-primary-50 focus:outline-none focus:shadow-outline focus:border-contraption-primary-300 md:py-4 md:text-lg md:px-10"
+                  >
+                    Submit a restaurant
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <svg
+            class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+        </div>
+      </div>
+      <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://images.unsplash.com/photo-1570894324081-04e0365bec41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+
+    <div>
+      <nuxt />
+    </div>
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    }
+  }
 }
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,86 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    colors: {
+      'contraption-primary': { // purple
+        '050': '#E6E6FF',
+        100: '#C4C6FF',
+        200: '#A2A5FC',
+        300: '#8888FC',
+        400: '#7069FA',
+        500: '#5D55FA',
+        600: '#4D3DF7',
+        700: '#3525E6',
+        800: '#1D0EBE',
+        900: '#0C008C'
+      },
+      'contraption-secondary': { // teal
+        '050': '#EFFCF6',
+        100: '#C6F7E2',
+        200: '#8EEDC7',
+        300: '#65D6AD',
+        400: '#3EBD93',
+        500: '#27AB83',
+        600: '#199473',
+        700: '#147D64',
+        800: '#0C6B58',
+        900: '#014D40'
+      },
+      gray: { // blue-gray
+        '050': '#F0F4F8',
+        100: '#D9E2EC',
+        200: '#BCCCDC',
+        300: '#9FB3C8',
+        400: '#829AB1',
+        500: '#627D98',
+        600: '#486581',
+        700: '#334E68',
+        800: '#243B53',
+        900: '#102A43'
+      },
+      blue: { // light blue vivid - for accents
+        '050': '#E3F8FF',
+        100: '#B3ECFF',
+        200: '#81DEFD',
+        300: '#5ED0FA',
+        400: '#40C3F7',
+        500: '#2BB0ED',
+        600: '#1992D4',
+        700: '#127FBF',
+        800: '#0B69A3',
+        900: '#035388'
+      },
+      red: { // red vivid - for accents
+        '050': '#FFE3E3',
+        100: '#FFBDBD',
+        200: '#FF9B9B',
+        300: '#F86A6A',
+        400: '#EF4E4E',
+        500: '#E12D39',
+        600: '#CF1124',
+        700: '#AB091E',
+        800: '#8A041A',
+        900: '#610316'
+      },
+      yellow: { // yellow vidid - for accents
+        '050': '#FFFBEA',
+        100: '#FFF3C4',
+        200: '#FCE588',
+        300: '#FADB5F',
+        400: '#F7C948',
+        500: '#F0B429',
+        600: '#DE911D',
+        700: '#CB6E17',
+        800: '#B44D12',
+        900: '#8D2B0B'
+      },
+      black: '#000000',
+      white: '#ffffff'
+    }
+  },
   variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/ui')
+  ]
 }
